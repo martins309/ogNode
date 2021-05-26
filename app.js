aa7e0ffa6b8852a7de612869c8d1873d10ac95aa
Andrew Martins <andrew_omartins@yahoo.com>
2021-05-25 22:02:21 -0400

    created a server although i dont know if it works lol

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
+'use strict'
+
+const http = require('http');
+
 const hostname = '127.0.0.1',
-    
+    port = 3001,
+    express = require('express'),
+    app = express();
+
+const cors = require('cors');
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preFlightContinue: false,
+    optionSuccessStatus: 204,
+    "Access-Control-Allow-Origin": '*',
+    "Access-Control-Allow-headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+}
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use(cors());
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}`)
+});
+
+const rootController = require('./routes/index')
+
+
+app.use('/', rootController);
